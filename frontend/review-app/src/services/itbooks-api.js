57f6bd1fa76a860c962beeeeafa100d2ab94d59e
liudmylai <liudmyla.iovenko@gmail.com>
2022-02-28 21:53:01 -0500

    added itbooks api

---
+++ frontend/review-app/src/services/itbooks-api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL = 'https://api.itbook.store/1.0/';
+
+function getAxios(endPoint) {
+    return axios.get(baseURL + endPoint)
+        .then(response => response.data)
+        .catch(error => console.error(error));
+}
+
+// search books by title, author, ISBN or keywords
+function searchBooks(query, page) {
+    const url = 'search/' + query + (page && '/' + page);
+    return getAxios(url);
+}
+
+// get new releases books
+function getNewBooks() {
+    return getAxios('new');
+}
+
+// get book details by ISBN
+function getBookByISBN(isbn) {
+    const url = 'books/' + isbn;
+    return getAxios(url);
+}
+
+export {searchBooks, getNewBooks, getBookByISBN};
