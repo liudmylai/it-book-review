b2dcf6f70a631a2f0c79b6fb42d1d61449bc7c95
liudmylai <liudmyla.iovenko@gmail.com>
2022-03-03 22:09:24 -0500

    update pagination

--- frontend/review-app/src/components/Pagination.js
+++ frontend/review-app/src/components/Pagination.js
@@ -1,29 +1,67 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { ReviewContext } from "../contexts/ReviewContext";
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ReviewContext } from '../contexts/ReviewContext';
 
 function Pagination() {
+    
     const { bookList } = useContext(ReviewContext);
-    const pages = [];
-    for (let i = 0, x = Math.ceil(bookList.total / 10); i < x; i++) {
-        pages[i] = i + 1;
 
+    const count = Math.ceil(bookList.total / 10);
+
+    function indexes(page, count) {
+        let start, end;
+        let result = [];
+
+        if (count < 10) {
+            for(let i = 1; i <= count; i++) {
+                result.push(i);
+            }
+            return result;
+        }
+
+        if (page < 6) {
+            start = 1;
+        } else {
+            start = Math.min(page - 2, count - 6);
+        }
+
+        if (page > (count - 5)) {
+            end = count;
+        } else {
+            end = Math.max(page + 2, 7);
+        }
+
+        for(let i = start; i <= end; i++) {
+            result.push(i);
+        }
+
+        if (start > 1) {
+            result = [1, '...', ...result];
+        }
+
+        if (end < count) {
+            result = [...result, '...', count];
+        }
+
+        return result;
     }
-
+    
     return (
         <div className='container'>
             <nav>
-                <ul className="pagination">
-                    <li className={bookList.page > 1 ? "page-item" : "page-item disabled"}><Link to={'/search/page-' + (Number(bookList.page) - 1)} className="page-link">Previous</Link></li>
-
-                    {pages.map((page, index) =>
-                    (page == bookList.page
-                        ?
-                        <li className="page-item active" key={index}><span className="page-link">{page}</span></li>
-                        :
-                        <li className="page-item" key={index}><Link to={'/search/page-' + page} className="page-link">{page}</Link></li>
-                    ))}
-                    <li className={bookList.page < pages.length ? "page-item" : "page-item disabled"}><Link to={'/search/page-' + (Number(bookList.page) + 1)} className="page-link">Next</Link></li>
+                <ul className='pagination'>
+                    <li className={bookList.page > 1 ? 'page-item' : 'page-item disabled'}><Link to={'/search/page-' + (Number(bookList.page) - 1)} className='page-link'>Previous</Link></li>
+                    {indexes(Number(bookList.page), count)
+                        .map((page, index) => {
+                            if (page == bookList.page) {
+                                return (<li className='page-item active' key={index}><span className='page-link'>{page}</span></li>);
+                            } else if (page === '...') {
+                                return (<li className='page-item disabled' key={index}><span className='page-link'>{page}</span></li>);
+                            } else {
+                                return (<li className='page-item' key={index}><Link className='page-link' to={'/search/page-' + page}>{page}</Link></li>);
+                            }
+                        })}
+                    <li className={bookList.page < count ? 'page-item' : 'page-item disabled'}><Link to={'/search/page-' + (Number(bookList.page) + 1)} className='page-link'>Next</Link></li>
                 </ul>
             </nav>
         </div>
