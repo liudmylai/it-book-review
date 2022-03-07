a369c461fed146f53355145e48bc5ae86ad1fb52
liudmylai <liudmyla.iovenko@gmail.com>
2022-03-06 19:25:42 -0500

    refactoring repository

--- review-backend/src/main/java/com/example/review/model/UserDetailsImpl.java
+++ review-backend/src/main/java/com/example/review/model/UserDetailsImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
 	private String userName;
