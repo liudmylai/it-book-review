6983918b4d60451d3de558dcef93de4962306a2c
liudmylai <liudmyla.iovenko@gmail.com>
2022-03-06 19:14:52 -0500

    refactoring repository

--- review-backend/src/main/java/com/example/review/ReviewBackendApplication.java
+++ review-backend/src/main/java/com/example/review/ReviewBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewApplication {
+public class ReviewBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewApplication.class, args);
+		SpringApplication.run(ReviewBackendApplication.class, args);
 	}
 
 }
